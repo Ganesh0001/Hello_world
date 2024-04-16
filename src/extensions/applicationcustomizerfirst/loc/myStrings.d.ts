f35e1e3fd33e84a636a9db53afd4d9f9884692e3
unknown <ganesh.patel1@upl-ltd.com>
2024-04-16 17:56:24 +0530

    Add CI/CD pipeline configuration

---
+++ src/extensions/applicationcustomizerfirst/loc/myStrings.d.ts
@@ -0,0 +1,8 @@
+declare interface IApplicationcustomizerfirstApplicationCustomizerStrings {
+  Title: string;
+}
+
+declare module 'ApplicationcustomizerfirstApplicationCustomizerStrings' {
+  const strings: IApplicationcustomizerfirstApplicationCustomizerStrings;
+  export = strings;
+}
